55af3a3582a262b6945fea39fa0c2c6ffaa06d58
Petr Pisl <petr.pisl@oracle.com>
2023-11-29 19:55:58 +0100

    Adding example.

---
+++ templates/jbang/examples/hello.java
@@ -0,0 +1,124 @@
+//REPOS mc=https://repo1.maven.org/maven2/
+//REPOS local=file:///home/petr/labs/standalone/repo/
+//DEPS org.graalvm.python:python-language:24.0.0-dev
+//DEPS org.graalvm.python:python-resources:24.0.0-dev
+//DEPS org.graalvm.python:python-launcher:24.0.0-dev
+//DEPS org.graalvm.python:python-embedding:24.0.0-dev
+//PIP termcolor
+
+
+
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.PolyglotAccess;
+import org.graalvm.polyglot.PolyglotException;
+import org.graalvm.polyglot.io.FileSystem;
+import org.graalvm.polyglot.io.IOAccess;
+import org.graalvm.python.embedding.utils.VirtualFileSystem;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.AccessMode;
+import java.nio.file.DirectoryStream;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.NotDirectoryException;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.FileTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.function.Predicate;
+
+public class graalpy2 {
+    public static void main(String[] args) {
+        System.out.println("Running main method from Java");
+        try (Context context = VirtualGraalPyContext.getContext()) {
+            switch (args.length) {
+                case 0:
+                    context.eval("python", "Hello from Python");
+                    break;
+                case 1:
+                    context.eval("python", args[0]);
+                    break;
+                default:
+                    throw new IllegalArgumentException("The main() helper only takes 0-1 arguments.");
+            }
+        } catch (PolyglotException e) {
+            if (e.isExit()) {
+                System.exit(e.getExitStatus());
+            } else {
+                throw e;
+            }
+        }
+    }
+}
+
+final class VirtualGraalPyContext {
+    private static final String VENV_PREFIX = "/vfs/venv";
+    private static final String HOME_PREFIX = "/vfs/home";
+
+    public static Context getContext() {
+        VirtualFileSystem vfs = VirtualFileSystem.create();
+        var builder = Context.newBuilder()
+            // set true to allow experimental options
+            .allowExperimentalOptions(false)
+            // deny all privileges unless configured below
+            .allowAllAccess(false)
+            // allow access to the virtual and the host filesystem, as well as sockets
+            .allowIO(IOAccess.newBuilder()
+                            .allowHostSocketAccess(true)
+                            .fileSystem(vfs)
+                            .build())
+            // allow creating python threads
+            .allowCreateThread(true)
+            // allow running Python native extensions
+            .allowNativeAccess(true)
+            // allow exporting Python values to polyglot bindings and accessing Java from Python
+            .allowPolyglotAccess(PolyglotAccess.ALL)
+            // choose the backend for the POSIX module
+            .option("python.PosixModuleBackend", "java")
+            // equivalent to the Python -B flag
+            .option("python.DontWriteBytecodeFlag", "true")
+            // equivalent to the Python -v flag
+            .option("python.VerboseFlag", System.getenv("PYTHONVERBOSE") != null ? "true" : "false")
+            // log level
+            .option("log.python.level", System.getenv("PYTHONVERBOSE") != null ? "FINE" : "SEVERE")
+            // equivalent to setting the PYTHONWARNINGS environment variable
+            .option("python.WarnOptions", System.getenv("PYTHONWARNINGS") == null ? "" : System.getenv("PYTHONWARNINGS"))
+            // print Python exceptions directly
+            .option("python.AlwaysRunExcepthook", "true")
+            // Force to automatically import site.py module, to make Python packages available
+            .option("python.ForceImportSite", "true")
+            // The sys.executable path, a virtual path that is used by the interpreter to discover packages
+            .option("python.Executable", vfs.resourcePathToPlatformPath(VENV_PREFIX) + (VirtualFileSystem.isWindows() ? "\\Scripts\\python.exe" : "/bin/python"))
+            // Do not warn if running without JIT. This can be desirable for short running scripts
+            // to reduce memory footprint.
+            .option("engine.WarnInterpreterOnly", "false");
+        if (System.getProperty("org.graalvm.nativeimage.imagecode") != null) {
+            // Set the python home to be read from the embedded resources
+            builder.option("python.PythonHome", vfs.resourcePathToPlatformPath(HOME_PREFIX));
+        }
+        return builder.build();
+    }
+}
